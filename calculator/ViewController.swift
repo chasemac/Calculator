18fdf6831687a75200ed2d6391f016f4470aad80
Chase McElroy <ichasem@gmail.com>
2017-05-02 13:30:05 -0700

    Set up basic ui and ViewController for working calculator

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,15 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text
+            display.text = textCurrentlyInDisplay! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbal = sender.currentTitle {
+            switch mathematicalSymbal {
+            case "π":
+                displayValue = Double.pi
+                case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
